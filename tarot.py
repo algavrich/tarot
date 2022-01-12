42dd877673f09834a26cf95d6c0a803e94283515
ALG <96560662+algavrich@users.noreply.github.com>
2022-01-12 14:11:25 -0800

    Major Arcana

---
+++ tarot.py
@@ -0,0 +1,24 @@
+import random
+deck = {
+	0: {"name": "The Fool", "meaning": {"Upright": "Beginnings, Innocence, Spontaneity, A Free Spirit", "Reversed": "Holding Back, Recklessness, Risk Taking"}}
+	1: {"name": "The Magician", "meaning": {"Upright": "Manifestation, Resourcefulness, Power, Inspired Action", "Reversed": "Manipulation, Poor Planning, Untapped Talents"}}
+	2: {"name": "The High Priestess", "meaning": {"Upright": "Intuition, Sacred Knowledge, Divine Feminine, The Subconscious Mind", "Reversed": "Secrets, Disconnected from Intuition, Withdrawl and Silence"}}
+	3: {"name": "The Empress", "meaning": {"Upright": "Femininity, Beauty, Nature, Nurturing, Abundance", "Reversed": "Creative Block, Dependence on Others"}}
+	4: {"name": "The Emperor", "meaning": {"Upright": "Authority, Establishment, Structure, A Father Figure", "Reversed": "Domination, Excessive Control, Lack of Discipline, Inflexibility"}}
+	5: {"name": "The Heirophant", "meaning": {"Upright": "Spiritual Wisdom, Religious Beliefs, Conformity, Tradition, Institutions", "Reversed": "Personal Beliefs, Freedom, Challenging the Status Quo"}}
+	6: {"name": "The Lovers", "meaning": {"Upright": "Love, Harmony, Relationships, Values Alignment, Choices", "Reversed": "Self Love, Disharmony, Imbalance, Misalignment of Values"}}
+	7: {"name": "The Chariot", "meaning": {"Upright": "Control, Willpower, Success, Action, Determination", "Reversed": "Self Discipline, Opposition, Lack of Direction"}}
+	8: {"name": "Strength", "meaning": {"Upright": "Strength, Courage, Persuasion, Influence, Compassion", "Reversed": "Inner Strength, Self Doubt, Low Energy, Raw Emotion"}}
+	9: {"name": "The Hermit", "meaning": {"Upright": "Soul Searching, Introspection, Being Alone, Inner Guidance", "Reversed": "Isolation, Loneliness, Withdrawl"}}
+	10: {"name": "Wheel of Fortune", "meaning": {"Upright": "Good Luck, Karma, Life Cycles, Destiny, A Turning Point", "Reversed": "Bad Luck, Resistance to Change, Breaking Cycles"}}
+	11: {"name": "Justice", "meaning": {"Upright": "Justice, Fairness, Truth, Cause and Effect, Law", "Reversed": "Unfairness, Lack of Accountability, Dishonesty"}}
+	12: {"name": "The Hanged Man", "meaning": {"Upright": "Pause, Surrender, Letting Go, New Perspectives", "Reversed": "Delays, Resistance, Stalling, Indecision"}}
+	13: {"name": "Death", "meaning": {"Upright": "Endings, Change, Transformation, Transition", "Reversed": "Resistance to Change, Personal Transformation, Inner Purging"}}
+	14: {"name": "Temperance", "meaning": {"Upright": "Balance, Moderation, Patience, Purpose", "Reversed": "Imbalance, Excess, Self Healing, Re-Alignment"}}
+	15: {"name": "The Devil", "meaning": {"Upright": "Shadow Self, Attachment, Addiction, Restriction, Sexuality", "Reversed": "Releasing Limiting Beliefs, Exploring Dark Thoughts, Detachment"}}
+	16: {"name": "The Tower", "meaning": {"Upright": "Sudden Change, Upheaval, Chaos, Revelation, Awakening", "Reversed": "Personal Transformation, Fear of Change, Averting Disaster"}}
+	17: {"name": "The Star", "meaning": {"Upright": "Hope, Faith, Purpose, Renewal, Spirituality", "Reversed": "Lack of Faith, Despair, Self Trust, Disconnection"}}
+	18:
+	19:
+	20:
+}
